cc792197a9c52293c3c65535d496353ef935253e
Jake Siegers <sirtopeia@yahoo.com>
2014-12-31 22:38:27 -0600

    Added Init files. Testing Build Script

---
+++ jenkins-builder-php/build.php
@@ -0,0 +1,93 @@
+<?php
+	class build{
+		function deploy(){
+			$cmdArgs = array(
+				"-avz",
+				"-e",
+				"'ssh -i /var/lib/jenkins-php-libs/sshKeys/jakesiegers'",
+				//"--delete",
+				//"--exclude=jenkins-builder-php/",
+				//"--exclude=project-files/",
+				//"--exclude=.gitignore/",
+				//"--exclude=.git/",
+				".",
+				"u43111589@jakesiegers.com:/jakeisa.ninja/zebra/"
+			);
+			print_r($cmdArgs);
+			$rsyncCommand="rsync ".implode(" ",$cmdArgs);
+			print_r($rsyncCommand);
+			$output;
+			$returnVar;
+			exec($rsyncCommand,$output,$returnVar);
+			echo implode("\n",$output);
+			return $this->exitCode($returnVar);
+		}
+
+		private function exitCode($returnVar){
+			switch($returnVar){
+				case 0:
+					echo 'Success';
+					break;
+				case 1:
+					echo 'Syntax or usage error';
+					break;
+				case 2:
+					echo 'Protocol incompatibility';
+					break;
+				case 3:
+					echo 'Errors selecting input/output files, dirs';
+					break;
+				case 4:
+					echo 'Requested  action not supported: an attempt was made to manipulate 64-bit files on a platform that cannot support them; or an option was specified that is supported by the client and not by the server.';
+					break;
+				case 5:
+					echo 'Error starting client-server protocol';
+					break;
+				case 6:
+					echo 'Daemon unable to append to log-file';
+					break;
+				case 10:
+					echo 'Error in socket I/O';
+					break;
+				case 11:
+					echo 'Error in file I/O';
+					break;
+				case 12:
+					echo 'Error in rsync protocol data stream';
+					break;
+				case 13:
+					echo 'Errors with program diagnostics';
+					break;
+				case 14:
+					echo 'Error in IPC code';
+					break;
+				case 20:
+					echo 'Received SIGUSR1 or SIGINT';
+					break;
+				case 21:
+					echo 'Some error returned by waitpid()';
+					break;
+				case 22:
+					echo 'Error allocating core memory buffers';
+					break;
+				case 23:
+					echo 'Partial transfer due to error';
+					break;
+				case 24:
+					echo 'Partial transfer due to vanished source files';
+					break;
+				case 25:
+					echo 'The --max-delete limit stopped deletions';
+					break;
+				case 30:
+					echo 'Timeout in data send/receive';
+					break;
+				case 35:
+					echo 'Timeout waiting for daemon connection';
+					break;
+			}
+			echo "\n";
+			return $returnVar;
+		}
+	}
+?>
