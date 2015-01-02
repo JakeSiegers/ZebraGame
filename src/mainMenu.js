05ef3ba25d785bccc5b6d9ce9a6de13072fb3a7b
Jake Siegers <sirtopeia@yahoo.com>
2015-01-02 00:11:53 -0600

    Removed temp images. Updated Menu

--- src/mainMenu.js
+++ src/mainMenu.js
@@ -1,11 +1,13 @@
 GameStateObj.MainMenu = function(game) {
 	menuMusic = null;
-	menuText = { font: "65px Arial", fill: "#000000", align: "center" };
+	menuTextStyle = { font: "65px Arial", fill: "#000000", align: "center" };
+	menuText = null;
 };
 GameStateObj.MainMenu.prototype = {
 	create: function() {
 		this.game.stage.backgroundColor = '#ff0000';
-		//this.game.add.text(0, 0, "Main Menu", style);
+		menuText = this.game.add.text(this.game.canvas.width/2, this.game.canvas.height/2, "Main Menu, Lame!", menuTextStyle);
+		menuText.anchor.set(0.5);
 		//this.add.sprite(0, 0, 'screen-mainmenu');
 		//this.add.sprite((320-221)/2, 40, 'title');
 		//this.startButton = this.add.button((320-146)/2, 200, 'button-start', this.startGame, this, 1, 0, 2);
@@ -13,16 +15,14 @@ GameStateObj.MainMenu.prototype = {
 		// 	60, 250, "Use arrow keys on desktop, \n  accelerometer on mobile",
 		// 	{ font: "16px Arial", fill: "#b921fe", stroke: "#22053a", strokeThickness: 3 }
 		// );
-		menuMusic = this.game.add.audio('bgm');
-		console.log("Playing music");
-		menuMusic.play();
+		//menuMusic = this.game.add.audio('bgm');
+		//console.log("Playing music");
+		//menuMusic.play();
 	},
-	/*
 	startGame: function() {
 		this.game.state.start('StoryHowto');
 	},
-	*/
 	render: function() {
-    	this.game.debug.soundInfo(menuMusic, 20, 32);
+    	//this.game.debug.soundInfo(menuMusic, 20, 32);
 	}
 };
--- src/preloader.js
+++ src/preloader.js
@@ -10,10 +10,14 @@ GameStateObj.Preloader.prototype = {
 		loadingText.anchor.set(0.5);
 		this.load.onFileComplete.add(this.updateLoadText);
 
+		//ADD ART TO LOAD HERE
+		//Your loader will take care of thuis automagically.
+
 		//this.preloadBg = this.add.sprite((320-297)/2, (480-145)/2, 'preloaderBg');
 		//this.preloadBar = this.add.sprite((320-158)/2, (480-50)/2, 'preloaderBar');
 		//this.load.setPreloadSprite(this.preloadBar);
-		
+
+		/*
 		this.load.image('ball', 'img/ball.png');
 		this.load.image('hole', 'img/hole.png');
 		this.load.image('element-w', 'img/element-w.png');
@@ -25,10 +29,11 @@ GameStateObj.Preloader.prototype = {
 		this.load.image('screen-bg', 'img/screen-bg.png');
 		this.load.image('screen-mainmenu', 'img/screen-mainmenu.png');
 		this.load.image('screen-howtoplay', 'img/screen-howtoplay.png');
+		*/
 
-		this.load.spritesheet('button-audio', 'img/button-audio.png', 35, 35);
+		//this.load.spritesheet('button-audio', 'img/button-audio.png', 35, 35);
 
-		this.load.audio('bgm', ['music/Paradisco.ogg']);
+		//this.load.audio('bgm', ['music/Paradisco.ogg']);
 	},
 	create: function() {
 		this.game.state.start('MainMenu');
