3746e6cf3eb44561bbdb5a827d719ccba5d59b08
Jake Siegers <sirtopeia@yahoo.com>
2015-01-13 20:08:44 -0600

    Updated Screen Size

--- src/start.js
+++ src/start.js
@@ -1,5 +1,5 @@
 (function() {
-	var game = new Phaser.Game(1136,640, Phaser.CANVAS, '');
+	var game = new Phaser.Game(1280,720, Phaser.CANVAS, '');
 	game.state.add('Boot', GameStateObj.Boot);
 	game.state.add('Preloader', GameStateObj.Preloader);
 	game.state.add('MainMenu', GameStateObj.MainMenu);
